72b2f7c8ce966c79be2a8c4a88ea73da94609dfc
pranshu11 <pranshu11@bitbucket.org>
2019-02-15 12:45:13 -0800

    React commit

---
+++ src/main/resources/static/precache-manifest.9d9d6444615ad05d7c5414771497f00b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a051c786a7882dde70e6",
+    "url": "/static/js/main.a051c786.chunk.js"
+  },
+  {
+    "revision": "f818fea8cbc9c0e5ac41",
+    "url": "/static/js/1.f818fea8.chunk.js"
+  },
+  {
+    "revision": "a051c786a7882dde70e6",
+    "url": "/static/css/main.c848ecd0.chunk.css"
+  },
+  {
+    "revision": "f818fea8cbc9c0e5ac41",
+    "url": "/static/css/1.1ae4fcf0.chunk.css"
+  },
+  {
+    "revision": "3c08b92a8b3e236e80f70c6cdb596973",
+    "url": "/index.html"
+  }
+];
